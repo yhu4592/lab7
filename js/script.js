86026beffd2486c4519ab9c6fd83b7390ff06c95
yhu4592 <hu.yongkangsg114@gmail.com>
2021-04-07 18:03:29 -0500

    Added event handler to alert input text and display it in heading below.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 function init(){
-    //add your javascrip between these two lines of code
-     
-    
-    
-    
-    
-    
-    
-    
+  //add your javascrip between these two lines of code
+  var entrybutton = document.getElementById('entrybutton');
+  entrybutton.addEventListener('click', function(){
+    var text = "Yongkang Hu: " + document.getElementById('entryinput').value;
+    alert(text);
+    document.getElementById('textoutput').innerHTML = document.getElementById('entryinput').value;   
+  });    
+}    
 window.addEventListener('load', init);
